c34107c0ba2c85db950fa66b58e3a054dae76ed3
yamsinator <BuiSteven@gmail.com>
2024-03-31 22:56:11 -0700

    Adding files

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,43 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+	
+	 private static final String URL = "jdbc:mysql://localhost:3306/world";
+	    private static final String USERNAME = "root";
+	    private static final String PASSWORD = "La911200";
+
+	    public static Connection getConnection() {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+	        } catch (ClassNotFoundException | SQLException e) {
+	            e.printStackTrace();
+	            return null;
+	        }
+	    }
+    
+    public static void main(String[] args) {
+    	
+    	try {
+			Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			System.out.println("Connected to Database!");
+			Statement stmt = con.createStatement();
+			ResultSet result = stmt.executeQuery("SELECT * FROM login WHERE id < 5");
+			
+			while(result.next()) {
+					System.out.println(result.getInt(1) + " " + result.getString(2) + " " + result.getString(3));				
+			}
+			con.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+    }
+}
+
---
+++ src/LoginForm.java
@@ -0,0 +1,124 @@
+package src;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class LoginForm extends JFrame {
+
+    public LoginForm() {
+        setTitle("User Login");
+        setSize(300, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField(15);
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField(15);
+        JButton loginButton = new JButton("Login");
+        
+        // Adding signUp link for new users
+        JLabel signUpLabel = new JLabel("<html><u>Sign Up</u></html>");
+        signUpLabel.setForeground(Color.blue.darker());
+        signUpLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        signUpLabel.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		// Implement action for sign up.
+        		openSignUpForm();
+        	}
+        });      
+        
+        // Adding forgot password link for current users that forgot or wish to change their password.
+        JLabel forgotPassLabel = new JLabel("<html><u>Forgot Password?</u></html>");
+        forgotPassLabel.setForeground(Color.blue.darker());
+        forgotPassLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        forgotPassLabel.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		// Implement action for forgot password.
+        	}
+        });
+        
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (UserAuthenticationSystem.authenticateUser(username, password)) {
+                    CustomerInfoForm customerInfoForm = new CustomerInfoForm(username);
+                    customerInfoForm.setVisible(true);
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password.");
+                }
+            }
+        });
+        
+        // Created GroupLayout so that each label can be properly aligned.
+        
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+        hGroup.addGroup(layout.createParallelGroup()
+                .addComponent(usernameLabel)
+                .addComponent(passwordLabel)
+                .addComponent(signUpLabel));
+        hGroup.addGroup(layout.createParallelGroup()
+                .addComponent(usernameField)
+                .addComponent(passwordField)
+                .addComponent(forgotPassLabel)
+                .addComponent(loginButton));
+        layout.setHorizontalGroup(hGroup);
+
+        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(usernameLabel)
+                .addComponent(usernameField));
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(passwordLabel)
+                .addComponent(passwordField));
+        vGroup.addComponent(loginButton);
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(signUpLabel)
+                .addComponent(forgotPassLabel));
+        layout.setVerticalGroup(vGroup);
+    }
+    
+    private void openSignUpForm() {
+    	SwingUtilities.invokeLater(() -> {
+    		SignUpForm signUpForm = new SignUpForm();
+    		signUpForm.setVisible(true);
+    	});
+    }
+    
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                LoginForm loginForm = new LoginForm();
+                loginForm.setVisible(true);
+            }
+        });
+    }
+    
+}
+
