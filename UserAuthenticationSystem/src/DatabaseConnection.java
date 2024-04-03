e50cff31a0461d37105589702acd48b7fe560a19
yamsinator <BuiSteven@gmail.com>
2024-04-02 22:24:14 -0700

    Removed database info from file so user can use their own.

--- UserAuthenticationSystem/src/DatabaseConnection.java
+++ UserAuthenticationSystem/src/DatabaseConnection.java
@@ -7,46 +7,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnection {
-	
-	 private static final String URL = "jdbc:mysql://localhost:3306/world";
-	 private static final String USERNAME = "root";
-	 private static final String PASSWORD = "La911200";
 
-	    public static Connection getConnection() {
-	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-	        } catch (ClassNotFoundException | SQLException e) {
-	            e.printStackTrace();
-	            return null;
-	        }
-	    }
-	    
-	    public static void main(String[] args) {
-	    	try {
+	private static final String URL = "jdbc:mysql://YOUR_HOSTNAME_HERE:3306/YOUR_SCHEMA_HERE";
+	private static final String USERNAME = "YOUR_USERNAME";
+	private static final String PASSWORD = "YOUR_PASSWORD";
 
-	    		java.sql.Connection con;
-	    		Class.forName("com.mysql.cj.jdbc.Driver");
-	    		con = DriverManager.getConnection(URL, USERNAME, PASSWORD); 
-
-	    		Statement stmt = con.createStatement();
-	    		
-	    		/* out.println(db + " database successfully opened.<br/><br/>"); */
-
-	    		/* Change name of table in this line */
-	    	    ResultSet rs = stmt.executeQuery("SELECT * FROM login WHERE id < 5;");
-	    	    while (rs.next()) {
-	                System.out.println(rs.getInt(1) + "    " + rs.getString(2) + "    "  + rs.getString(3) + "<br/><br/>");
-	                
-	                
-	              }
-	            rs.close();
-	            stmt.close();
-	            con.close();
-	    	}
-	    	catch (Exception e) {
-	    		System.out.println(e);
-	    	}
-	    }
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
-
